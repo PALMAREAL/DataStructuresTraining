6b63822927273349a96cdbeec26ca64ffabf99d9
Haliam Pérez <haliamp@yahoo.es>
2020-09-01 16:00:22 +0100

    Calling ProcessList method from Program

--- Algorithm.UI/Program.cs
+++ Algorithm.UI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Algorithm.Library;
+using System;
+using System.Collections.Generic;
 
 namespace Algorithm.UI
 {
@@ -6,7 +8,14 @@ namespace Algorithm.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Algorithm 01
+            var input = new List<int>() { -4, 7, 2, 9, -5, 8, 22, 1 };
+
+            var result = Algorithms.ProcessList(input, 5);
+
+            foreach (var item in result)
+                Console.WriteLine(item);
+            
         }
     }
 }
