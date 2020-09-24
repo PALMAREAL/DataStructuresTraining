dc8a7abbec9a2bb66956a366dbf2a4f486e3c318
Haliam Pérez <haliamp@yahoo.es>
2020-09-24 18:02:54 +0100

    Add Equals to Player pending Implementing Add somes problems to Linq Add somes test to LinqTest

--- Algorithm.Library/LinQ/Linq.cs
+++ Algorithm.Library/LinQ/Linq.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
 
 namespace Algorithm.Library.LinQ
 {
@@ -15,6 +18,12 @@ namespace Algorithm.Library.LinQ
             Data = data;
         }
 
+
+        /// <summary>
+        /// Confirm if exist a player olther than...
+        /// </summary>
+        /// <param name="age"></param>
+        /// <returns></returns>
         public bool ExistPlayerOlderThan(int age)
         {
             return Data.Any(player => GetAge(player.Birthday) > age);
@@ -25,6 +34,10 @@ namespace Algorithm.Library.LinQ
             return DateTime.Now.Year - birthday.Year;
         }
 
+        /// <summary>
+        /// Count all femmale players
+        /// </summary>
+        /// <returns></returns>
         public int CountFemmalePlayers()
         {
             //return Data.Count(player => player.Gender.ToString().ToUpper() == "F");
@@ -32,11 +45,105 @@ namespace Algorithm.Library.LinQ
             return Data.Where(player => player.Gender.ToString().ToUpper() == "F").Count();
         }
 
+        /// <summary>
+        /// Confirm if exist a Name in players list.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public bool ExistPlayerNamed(string name)
         {
-            return Data.Any(player => 
+            return Data.Any(player =>
                 player.Name.Trim().ToLower() == name.Trim().ToLower() ||
                 player.Surname.Trim().ToLower() == name.Trim().ToLower());
+        }
+
+        /// <summary>
+        /// Get the initial position for a player
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public int PlayerPositionInitialList(string name)
+        {
+            return Data.ToList().FindIndex(player =>
+            player.Name.Trim().ToLower() == name.Trim().ToLower() ||
+            player.Surname.Trim().ToLower() == name.Trim().ToLower());
+        }
+
+        /// <summary>
+        /// Get the ranking position
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="elo"></param>
+        /// <returns></returns>
+        public int PlayerRankingPosition(string name, int elo)
+        {
+            //Seleccionar todos los jugadores por encima del elo de Kasparov
+            //Ordenar
+            //Devolver la posición de Kasparov en esa lista
+
+            return Data.OrderBy(player => player.Elo).ToList().FindIndex(player =>
+            player.Elo == elo);
+            //player.Name.Trim().ToLower() == name.Trim().ToLower() ||
+            //player.Surname.Trim().ToLower() == name.Trim().ToLower());
+        }
+
+        /// <summary>
+        /// Calculate Elo average from all players
+        /// </summary>
+        /// <returns></returns>
+        public int PlayersEloAverage()
+        {
+            return (int)Data.Average(player => player.Elo);
+        }
+
+        /// <summary>
+        /// Calculate weight from a range off elo
+        /// </summary>
+        /// <returns></returns>
+        //public double CalculateTotalWeight()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        //public List<Player> SortAscByBirthday()
+        //{
+        //    throw new NotImplementedException;
+        //}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string EvaluateMenWomenProportion()
+        {
+            int menPlayers = Data.Where(player => player.Gender.ToString().ToUpper() == "M").Count();
+            int womenPlayers = Data.Where(player => player.Gender.ToString().ToUpper() == "F").Count();
+
+            if (menPlayers == womenPlayers)
+                return "Equals";
+
+            return menPlayers > womenPlayers ? "Men" : "Women";
+        }
+
+        /// <summary>
+        /// Get Masculin players with A or J or G as initial character in name.
+        /// </summary>
+        /// <returns></returns>
+        public List<Player> PlayersNameWithConditions()
+        {
+            return Data.Where(player => player.Gender.ToString().ToUpper() == "M" &&
+            (player.Name.StartsWith('A') ||
+            player.Name.StartsWith('J') ||
+            player.Name.StartsWith('G'))).ToList();
+        }
+
+        public List<Player> PlayersSurnamedWithConditions()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Algorithm.Test/LinqTest.cs
+++ Algorithm.Test/LinqTest.cs
@@ -17,23 +17,56 @@ namespace Algorithm.Test
             var data = new List<Player>
             {
                 new Player
-                { 
-                    Name = "Anatoly", 
-                    Surname = "Karpov", 
-                    Gender = 'M', 
-                    Birthday = new DateTime(1951,2,14), 
-                    Weight = 81.4, 
+                {
+                    Name = "Anatoly",
+                    Surname = "Karpov",
+                    Gender = 'M',
+                    Birthday = new DateTime(1951,2,14),
+                    Weight = 81.4,
                     Elo = 2617
                 },
-                new Player{ Name = "Judith", Surname = "Polgar", Gender = 'F', Birthday = new DateTime(1976,2,11), Weight = 70.7, Elo = 2646},
-                new Player{ Name = "Garry", Surname = "Kasparov", Gender = 'm', Birthday = new DateTime(1963,7,4), Weight = 64.2, Elo = 2812},
-                new Player{ Name = "Hou", Surname = "Yifan", Gender = 'f', Birthday = new DateTime(1994,8,23), Weight = 50.0, Elo = 2658},
-                new Player{ Name = "  MaGnus", Surname = "Carlsen", Gender = 'M', Birthday = new DateTime(1990,6,28), Weight = 67.9, Elo = 2863}
+                new Player
+                { 
+                    Name = "Judith", 
+                    Surname = "Polgar", 
+                    Gender = 'F', 
+                    Birthday = new DateTime(1976,2,11), 
+                    Weight = 70.7, 
+                    Elo = 2646
+                },
+                new Player
+                { 
+                    Name = "Garry", 
+                    Surname = "Kasparov", 
+                    Gender = 'm', 
+                    Birthday = new DateTime(1963,7,4), 
+                    Weight = 64.2, 
+                    Elo = 2812
+                },
+                new Player
+                { 
+                    Name = "Hou", 
+                    Surname = "Yifan", 
+                    Gender = 'f', 
+                    Birthday = new DateTime(1994,8,23), 
+                    Weight = 50.0, 
+                    Elo = 2658
+                },
+                new Player
+                { 
+                    Name = "  MaGnus", 
+                    Surname = "Carlsen", 
+                    Gender = 'M', 
+                    Birthday = new DateTime(1990,6,28), 
+                    Weight = 67.9, 
+                    Elo = 2863
+                }
             };
 
             Query = new Linq(data);
         }
 
+        //1
         [Fact]
         public void Exist_Player_Older_Than()
         {
@@ -42,6 +75,7 @@ namespace Algorithm.Test
             Assert.False(result);
         }
 
+        //2
         [Fact]
         public void Count_Femmale_Players()
         {
@@ -50,12 +84,107 @@ namespace Algorithm.Test
             Assert.Equal(2, result);
         }
 
+        //3
         [Fact]
         public void Exist_Player_Named()
         {
             bool result = Query.ExistPlayerNamed("Magnus");
 
             Assert.True(result);
+        }
+
+        //4
+        [Fact]
+        public void Player_Position_Initial_List()
+        {
+            int result = Query.PlayerPositionInitialList("Kasparov");
+
+            Assert.Equal(2, result);
+        }
+
+        //5
+        [Fact]
+        public void Player_Ranking_Position()
+        {
+            int result = Query.PlayerRankingPosition("Kasparov", 2812);
+
+            Assert.Equal(2, result);
+        }
+
+        //6
+        [Fact]
+        public void Players_Elo_Average()
+        {
+            int result = Query.PlayersEloAverage();
+
+            Assert.Equal(2719, result);
+        }
+
+        //7
+        //[Fact]
+        //public void Calculate_Total_Weight()
+        //{
+        //    double result = Query.CalculateTotalWeight();
+
+        //    Assert.Equal(202.1, result);
+        //}
+
+        //8 
+        //[Fact]
+        //public void Surname_And_Elo_SortAsc_ByBirthday()
+        //{
+        //    List<Player> result = Query.SortAscByBirthday();
+
+        //    List<Player> expected = new List<Player>() 
+        //    { 
+        //        new Player(){Surname = "Yifan", Elo = 2658},
+        //        new Player(){Surname = "Carlsen", Elo = 2863 },
+        //        new Player(){Surname = "Polgar", Elo = 2646 },
+        //        new Player(){Surname = "Garry", Elo = 2812 },
+        //        new Player(){Surname = "Karpov", Elo = 2617 }
+        //    };
+
+        //    Assert.Equal(expected, result);
+        //}
+
+        //9
+        [Fact]
+        public void Evaluate_Men_Women_Proportion()
+        {
+            string result = Query.EvaluateMenWomenProportion();
+
+            Assert.Equal("Men",result);
+        }
+
+        //10
+        [Fact]
+        public void Get_Players_With_Named_Conditions()
+        {
+            List<Player> result = Query.PlayersNameWithConditions();
+
+            List<Player> expected = new List<Player>()
+            {
+                new Player(){Name = "Anatoly"},
+                new Player(){Name = "Garry"}
+            };
+
+            Assert.Equal(expected, result);
+        }
+
+        //11
+        [Fact]
+        public void Get_Players_With_Surnamed_Conditions()
+        {
+            List<Player> result = Query.PlayersSurnamedWithConditions();
+
+            List<Player> expected = new List<Player>()
+            {
+                new Player(){Surname = "Karpov"},
+                new Player(){Surname = "Polgar"},
+                new Player(){Surname = "Kasparov"}
+            };
+
+            Assert.Equal(expected, result);
         }
     }
 }
